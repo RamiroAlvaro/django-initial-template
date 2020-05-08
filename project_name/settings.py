b1e76a3dc1be3350a0de2139e7b306e2b573cdb0
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-05-07 23:47:30 -0300

    install and config python-decouple

--- project_name/settings.py
+++ project_name/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from decouple import config, Csv
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -23,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+3ws(&i4scb658f-2u46#q6dzo3a_#@z((g5&_jg=uq%8g*$rh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
